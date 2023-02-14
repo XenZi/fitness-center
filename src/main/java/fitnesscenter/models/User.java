b1707c66e89c9c582406009871c98571e6ca55e5
Vaaragh <vaarghl@gmail.com>
2023-02-14 23:18:57 +0100

    feat: partial user class added

---
+++ src/main/java/fitnesscenter/models/User.java
@@ -0,0 +1,5 @@
+package fitnesscenter.models;
+
+public class User {
+
+}
--- src/main/java/fitnesscenter/service/AuthService.java
+++ src/main/java/fitnesscenter/service/AuthService.java
@@ -1,6 +1,14 @@
 package fitnesscenter.service;
 
+import javax.servlet.http.HttpSession;
+
 import fitnesscenter.interfaces.service.IAuthService;
 
 public class AuthService implements IAuthService {
+
+	@Override
+	public void login(String username, String password, HttpSession session) {
+		// TODO Auto-generated method stub
+		
+	}
 }
