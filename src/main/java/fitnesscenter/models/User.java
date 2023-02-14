cd276eee34d0aa9688a8195ae53ebd74ed3679a8
Vaaragh <vaarghl@gmail.com>
2023-02-14 23:22:43 +0100

    feat: added user model

--- src/main/java/fitnesscenter/models/User.java
+++ src/main/java/fitnesscenter/models/User.java
@@ -1,5 +1,25 @@
 package fitnesscenter.models;
 
+import java.util.List;
+import java.util.TimeZone;
+
+import fitnesscenter.enums.ERole;
+
 public class User {
+	
+	private String id;
+	private String phoneNumber;
+	private String address;
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String email;
+	private String ccNumber;
+	private Language mainLanguage;
+	private List<Language> allLanguages;
+	private ERole role;
+	private TimeZone timezone;
+	
+	
 
 }
