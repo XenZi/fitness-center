3783b4cf7982ef5de09ea2d46b083dde98db950a
Vaaragh <vaarghl@gmail.com>
2023-02-15 02:18:45 +0100

    feat: Add WatchData repository

--- src/main/java/fitnesscenter/interfaces/repository/IWatchDataRepository.java
+++ src/main/java/fitnesscenter/interfaces/repository/IWatchDataRepository.java
@@ -1,5 +1,16 @@
 package fitnesscenter.interfaces.repository;
 
+import java.util.List;
+
+import fitnesscenter.models.WatchData;
+
 public interface IWatchDataRepository {
+	
+	public List<WatchData> findAll();
+	
+	public WatchData findOneById(String id);
+	
+	public void save(WatchData watchData);	
+	
 
 }
--- src/main/java/fitnesscenter/repository/WatchDataRepository.java
+++ src/main/java/fitnesscenter/repository/WatchDataRepository.java
@@ -2,16 +2,18 @@ package fitnesscenter.repository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import fitnesscenter.interfaces.repository.IWatchDataRepository;
 import fitnesscenter.models.WatchData;
 
 @Repository
-public class WatchDataRepository {
+public class WatchDataRepository implements IWatchDataRepository{
 
 	@Autowired
 	private JdbcTemplate db;
@@ -31,13 +33,27 @@ public class WatchDataRepository {
 			watchData.setHeartRate(heartRate);
 			watchData.setCalories(calories);
 			
-			
 			return watchData;
 		}
 		
-		
-		
-		
+	}
+
+	@Override
+	public List<WatchData> findAll() {
+		String sql = "SELECT id, heart_rate, calories FROM WatchData;";
+		return db.query(sql,  new RowMap());
+	}
+
+	@Override
+	public WatchData findOneById(String id) {
+		String sql = "SELECT id, heart_rate, calories FROM WatchData WHERE id=?;";
+		return db.queryForObject(sql, new RowMap(), id);
+	}
+
+	@Override
+	public void save(WatchData watchData) {
+		String sql = "INSERT INTO WatchData (id, heart_rate, calories) VALUES (?,?,?);";
+		db.update(sql, watchData.getId(), watchData.getHeartRate(), watchData.getCalories());
 	}
 	
 }
